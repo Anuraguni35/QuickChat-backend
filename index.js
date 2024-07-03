d460f6d3f6184ddc7e783db89c7e049325e11a00
Anurag Uniyal <anurag.uniyal@decobeetech.com>
2024-07-03 12:31:46 +0530

    -Added file

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
 // const express = require('express');
-import express from 'express'
-import dotenv from 'dotenv'
-import router from './routes/index.route.js';
+import express from "express";
+import dotenv from "dotenv";
+import router from "./routes/index.route.js";
 import connectToMongoDB from "./Db/connetToMongoDb.js";
 import morgan from "morgan";
 import cors from "cors";
-import {app,server} from './Socket/Socket.js'
+import { app, server } from "./Socket/Socket.js";
 import mongoose from "mongoose";
 // const app = express();
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 app.use(cors());
 dotenv.config();
 
+const Port = process.env.Port || 4000;
+app.use(express.json());
+app.use(router);
 
-const Port=process.env.Port||4000;
- app.use(express.json());
-app.use(router)
-
-console.log(process.env.Mongo_Db_URI, "FROM ENV");
-
-const MONGO_URL = process.env.Mongo_Db_URI || `mongodb+srv://anuraguniyal35:Power35@cluster0.70gaohf.mongodb.net/chatApp?retryWrites=true&w=majority`
-server.listen(Port,()=>{
-    mongoose.connect(MONGO_URL)
-    .then(() => {console.log("MONGO DB connected successfully.")
-        console.log("server listening on port "+Port)
+const MONGO_URL = process.env.Mongo_Db_URI;
+server.listen(Port, () => {
+  mongoose
+    .connect(MONGO_URL)
+    .then(() => {
+      console.log("MONGO DB connected successfully.");
+      console.log("server listening on port " + Port);
     })
-    .catch((err) => console.error("ERROR connecting MONGODB =>", err.message))
-    
+    .catch((err) => console.error("ERROR connecting MONGODB =>", err.message));
 });
